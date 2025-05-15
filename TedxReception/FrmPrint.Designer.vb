1dd6cfe33225918d5a000fb070fe4b18f3b6b88a
Ali7med <Ali.hammed.it@gmail.com>
2025-05-15 10:39:55 +0300

    20250515

---
+++ TedxReception/FrmPrint.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPrint
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TxtName = New System.Windows.Forms.TextBox()
+        Me.BtnPrint = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DGResult = New System.Windows.Forms.DataGridView()
+        Me.ColID = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ColName = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BtnCrap = New System.Windows.Forms.Button()
+        Me.BtnPrintDGV = New System.Windows.Forms.Button()
+        Me.CBPrinters = New System.Windows.Forms.ComboBox()
+        Me.BtnRefreshPrinter = New System.Windows.Forms.Button()
+        CType(Me.DGResult, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TxtName
+        '
+        Me.TxtName.Font = New System.Drawing.Font("Tahoma", 20.0!)
+        Me.TxtName.Location = New System.Drawing.Point(271, 36)
+        Me.TxtName.Name = "TxtName"
+        Me.TxtName.Size = New System.Drawing.Size(348, 48)
+        Me.TxtName.TabIndex = 0
+        Me.TxtName.Text = "كرار قاسم محمد جار الله"
+        Me.TxtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'BtnPrint
+        '
+        Me.BtnPrint.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.BtnPrint.Location = New System.Drawing.Point(107, 36)
+        Me.BtnPrint.Name = "BtnPrint"
+        Me.BtnPrint.Size = New System.Drawing.Size(158, 48)
+        Me.BtnPrint.TabIndex = 1
+        Me.BtnPrint.Text = "Print"
+        Me.BtnPrint.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.Label1.Location = New System.Drawing.Point(625, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 29)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "الاسم"
+        '
+        'DGResult
+        '
+        Me.DGResult.AllowUserToAddRows = False
+        Me.DGResult.AllowUserToDeleteRows = False
+        Me.DGResult.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DGResult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DGResult.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColID, Me.ColName})
+        Me.DGResult.Location = New System.Drawing.Point(38, 145)
+        Me.DGResult.Name = "DGResult"
+        Me.DGResult.ReadOnly = True
+        Me.DGResult.RowHeadersWidth = 51
+        Me.DGResult.RowTemplate.Height = 26
+        Me.DGResult.Size = New System.Drawing.Size(738, 479)
+        Me.DGResult.TabIndex = 3
+        '
+        'ColID
+        '
+        Me.ColID.HeaderText = "ID"
+        Me.ColID.MinimumWidth = 6
+        Me.ColID.Name = "ColID"
+        Me.ColID.ReadOnly = True
+        '
+        'ColName
+        '
+        Me.ColName.HeaderText = "Name"
+        Me.ColName.MinimumWidth = 6
+        Me.ColName.Name = "ColName"
+        Me.ColName.ReadOnly = True
+        '
+        'BtnCrap
+        '
+        Me.BtnCrap.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.BtnCrap.Location = New System.Drawing.Point(618, 630)
+        Me.BtnCrap.Name = "BtnCrap"
+        Me.BtnCrap.Size = New System.Drawing.Size(158, 48)
+        Me.BtnCrap.TabIndex = 1
+        Me.BtnCrap.Text = "Crap"
+        Me.BtnCrap.UseVisualStyleBackColor = True
+        '
+        'BtnPrintDGV
+        '
+        Me.BtnPrintDGV.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.BtnPrintDGV.Location = New System.Drawing.Point(38, 630)
+        Me.BtnPrintDGV.Name = "BtnPrintDGV"
+        Me.BtnPrintDGV.Size = New System.Drawing.Size(158, 48)
+        Me.BtnPrintDGV.TabIndex = 1
+        Me.BtnPrintDGV.Text = "Print"
+        Me.BtnPrintDGV.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.CBPrinters.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.CBPrinters.FormattingEnabled = True
+        Me.CBPrinters.Location = New System.Drawing.Point(211, 637)
+        Me.CBPrinters.Name = "ComboBox1"
+        Me.CBPrinters.Size = New System.Drawing.Size(218, 36)
+        Me.CBPrinters.TabIndex = 4
+        '
+        'BtnRefreshPrinter
+        '
+        Me.BtnRefreshPrinter.Font = New System.Drawing.Font("Tahoma", 14.0!)
+        Me.BtnRefreshPrinter.Location = New System.Drawing.Point(435, 636)
+        Me.BtnRefreshPrinter.Name = "BtnRefreshPrinter"
+        Me.BtnRefreshPrinter.Size = New System.Drawing.Size(51, 38)
+        Me.BtnRefreshPrinter.TabIndex = 1
+        Me.BtnRefreshPrinter.Text = "R"
+        Me.BtnRefreshPrinter.UseVisualStyleBackColor = True
+        '
+        'FrmPrint
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 681)
+        Me.Controls.Add(Me.CBPrinters)
+        Me.Controls.Add(Me.DGResult)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnCrap)
+        Me.Controls.Add(Me.BtnRefreshPrinter)
+        Me.Controls.Add(Me.BtnPrintDGV)
+        Me.Controls.Add(Me.BtnPrint)
+        Me.Controls.Add(Me.TxtName)
+        Me.Name = "FrmPrint"
+        Me.Text = "Tedx Reception"
+        CType(Me.DGResult, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TxtName As TextBox
+    Friend WithEvents BtnPrint As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents DGResult As DataGridView
+    Friend WithEvents ColID As DataGridViewTextBoxColumn
+    Friend WithEvents ColName As DataGridViewTextBoxColumn
+    Friend WithEvents BtnCrap As Button
+    Friend WithEvents BtnPrintDGV As Button
+    Friend WithEvents CBPrinters As ComboBox
+    Friend WithEvents BtnRefreshPrinter As Button
+End Class
---
+++ TedxReception/FrmPrint.vb
@@ -0,0 +1,81 @@
+﻿Public Class FrmPrint
+    Private Sub BtnPrint_Click(sender As Object, e As EventArgs) Handles BtnPrint.Click
+        print(id:=101, name:=TxtName.Text)
+    End Sub
+    Sub print(id As String, name As String)
+        Dim rpt = New Badge
+        Dim dt, dt2 As New DataTable
+        rpt.SetParameterValue("id", id)
+        rpt.SetParameterValue("name", name)
+
+        FrmPrintCR.CrystalReportViewer1.ReportSource = rpt
+        FrmPrintCR.ShowDialog()
+    End Sub
+    Private Sub BtnCrap_Click(sender As Object, e As EventArgs) Handles BtnCrap.Click
+        LoadFromClipboardToDataGridView(dgv:=DGResult)
+    End Sub
+    Private Sub LoadFromClipboardToDataGridView(dgv As DataGridView)
+        Try
+            ' Get text from clipboard
+            Dim clipboardText As String = Clipboard.GetText()
+            If String.IsNullOrWhiteSpace(clipboardText) Then
+                MessageBox.Show("Clipboard is empty or does not contain text.")
+                Exit Sub
+            End If
+
+            ' Clear existing rows
+            dgv.Rows.Clear()
+
+            ' Split into lines
+            Dim lines() As String = clipboardText.Split(New String() {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+
+            For Each line As String In lines
+                ' Split each line by tab
+                Dim parts() As String = line.Split(ControlChars.Tab)
+                If parts.Length >= 2 Then
+                    dgv.Rows.Add(parts(0).Trim(), parts(1).Trim())
+                End If
+            Next
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnPrintDGV_Click(sender As Object, e As EventArgs) Handles BtnPrintDGV.Click
+        For Each row As DataGridViewRow In DGResult.Rows
+            PrintSilently(id:=row.Cells("ColID").Value,
+                          name:=row.Cells("ColName").Value,
+                          printerName:=CBPrinters.SelectedItem.ToString())
+        Next
+    End Sub
+
+    Private Sub FrmPrint_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadPrinters()
+    End Sub
+    Private Sub LoadPrinters()
+        CBPrinters.Items.Clear()
+        For Each printer As String In System.Drawing.Printing.PrinterSettings.InstalledPrinters
+            CBPrinters.Items.Add(printer)
+        Next
+
+        If CBPrinters.Items.Count > 0 Then
+            CBPrinters.SelectedIndex = 0 ' Select default
+        End If
+    End Sub
+
+    Private Sub BtnRefreshPrinter_Click(sender As Object, e As EventArgs) Handles BtnRefreshPrinter.Click
+        LoadPrinters()
+    End Sub
+    Sub PrintSilently(id As String, name As String, printerName As String)
+        Try
+            Dim rpt As New Badge()
+            rpt.SetParameterValue("id", id)
+            rpt.SetParameterValue("name", name)
+
+            rpt.PrintOptions.PrinterName = printerName
+            rpt.PrintToPrinter(1, False, 0, 0)
+        Catch ex As Exception
+            MessageBox.Show("Printing failed: " & ex.Message)
+        End Try
+    End Sub
+End Class
